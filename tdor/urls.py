aa9402f93cababa2dd7b1a8ce7aaf82efcbd14fb
Brian Farrell <notkilroy@gmail.com>
2017-11-20 18:25:12 -0500

    Fixed slash direction for template.

--- tdor/urls.py
+++ tdor/urls.py
@@ -3,5 +3,5 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    url(r'^2017/', TemplateView.as_view(template_name='tdor\\2017.html'), name="tdor_2017"),
+    url(r'^2017/', TemplateView.as_view(template_name='tdor/2017.html'), name="tdor_2017"),
 ]
